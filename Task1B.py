ca56bbfbbcbbe159bd5edb5e0c0a86a96f11c65f
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-02-05 15:26:03 +0000

    Implement Task 1B demo program

--- Task1B.py
+++ Task1B.py
@@ -2,9 +2,20 @@
 #
 # SPDX-License-Identifier: MIT
 
+from floodsystem.geo import stations_by_distance
+from floodsystem.stationdata import build_station_list
+
+
 def main():
-    pass
+    stations = build_station_list()
+
+    by_distance = stations_by_distance(stations, (52.2053, 0.1218))
+    by_distance = [(station.name, station.town, dist) for station, dist in by_distance]
+
+    print(f"10 closest:\n{by_distance[:10]}")
+    print(f"---\n10 furthest:\n{by_distance[-10:]}")
 
 
 if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
     main()
