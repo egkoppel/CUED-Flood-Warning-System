be24cce9b7ee16fb83c5db44d72113ec92baaa38
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-02-05 14:57:33 +0000

    Stub out `stations_by_distance()`

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2018 Garth N. Wells
+# Copyright (C) 2025 Eliyahu Gluschove-Koppel
 #
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
@@ -6,4 +7,18 @@ geographical data.
 
 """
 
+from .station import MonitoringStation
 from .utils import sorted_by_key  # noqa
+
+
+def stations_by_distance(
+        stations: list[MonitoringStation],
+        p: tuple[float, float]
+) -> list[tuple[MonitoringStation, float]]:
+    """Calculates the distance from a list of stations to a given coordinate
+
+    Arguments:
+    stations -- list of `MonitoringStation` objects to calculate the distance to
+    p -- tuple of a coordinate to calculate the distance to
+    """
+    raise NotImplementedError("stub")
