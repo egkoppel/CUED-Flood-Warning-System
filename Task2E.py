afb274013fa86efa5e27c19e8aaa84b65dbc01f1
Mysha <ma2178@cam.ac.uk>
2025-02-28 15:35:02 +0000

    task 2e demo program

--- Task2E.py
+++ Task2E.py
@@ -1,1 +1,27 @@
+import datetime
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.plot import plot_water_levels
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest water level data
+    update_water_levels(stations)
+
+    # Get the 5 stations with the highest relative water level
+    top_stations = stations_highest_rel_level(stations, 5)
+
+    for station, _ in top_stations:
+        # Fetch water level data for past 10 days
+        dt = datetime.timedelta(days=10)
+        dates, levels = fetch_measure_levels(station.measure_id, dt)
+
+        # Plot water levels
+        plot_water_levels(station, dates, levels)
+
+if __name__ == "__main__":
+    run()
 
