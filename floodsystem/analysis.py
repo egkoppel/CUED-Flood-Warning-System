225c7e45065943993fabb5aa41966e260d6d9b18
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-03-01 16:06:04 +0000

    Add `polyfit` function

---
+++ floodsystem/analysis.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2025 Eliyahu Gluschove-Koppel
+#
+# SPDX-License-Identifier: MIT
+"""This module provides functions to analyse historical water level data
+"""
+import datetime
+
+from matplotlib.dates import date2num
+import numpy as np
+
+from floodsystem.utils import sorted_by_key
+
+
+def polyfit(dates, levels, p):
+    combined_sorted = sorted_by_key(zip(dates, levels), 1)
+    unzipped = list(zip(*combined_sorted))
+    dates = unzipped[0]
+    levels = unzipped[1]
+
+    dates = date2num(dates)
+    d0 = dates[0]
+    dates = dates - d0
+
+    p_coeff = np.polyfit(dates, levels, p)
+    poly = np.poly1d(p_coeff)
+
+    return poly, d0
