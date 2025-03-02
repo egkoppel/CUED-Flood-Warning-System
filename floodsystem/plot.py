9a6471f0f476d1a2043afb47fcd1f1c8cc375e18
Eliyahu Gluschove-Koppel <egluschovekoppel1@gmail.com>
2025-03-02 16:59:43 +0000

    Add `plot_water_level_with_fit`

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
-from datetime import datetime
-
+from floodsystem.analysis import polyfit
+import numpy as np
+from matplotlib.dates import date2num, num2date
 
 def _add_water_level_to_plot(station, dates, levels):
     if not dates or not levels:
@@ -17,6 +18,27 @@ def _add_water_level_to_plot(station, dates, levels):
 
 def plot_water_levels(station, dates, levels):
     _add_water_level_to_plot(station, dates, levels)
+
+    # Labels and title
+    plt.xlabel("Date")
+    plt.ylabel("Water Level (m)")
+    plt.xticks(rotation=45)
+    plt.title(station.name)
+    plt.legend()
+
+    # Ensure layout doesn't cut off labels
+    plt.tight_layout()
+
+    # Show plot
+    plt.show()
+
+
+def plot_water_level_with_fit(station, dates, levels, p):
+    _add_water_level_to_plot(station, dates, levels)
+    poly, d0 = polyfit(dates, levels, p)
+
+    d1 = np.linspace(d0, date2num(max(dates)), 30)
+    plt.plot(num2date(d1), poly(d1 - d0), label="Polynomial fit", linestyle='--')
 
     # Labels and title
     plt.xlabel("Date")
